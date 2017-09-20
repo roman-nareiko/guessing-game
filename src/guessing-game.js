d7921493cfb121aea241d55ed8f3b5a8018464b0
Roman Nareiko <roma.nareiko@gmail.com>
2017-09-21 00:11:46 +0300

    init

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = 0;
+        this._max = 0;
+        this._result = 0;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
+        this._result = Math.round(this._min + (this._max - this._min) / 2);
 
+        return this._result;
     }
 
     lower() {
-
+        this._max = this._result;   
     }
 
     greater() {
-
+        this._min = this._result;
     }
 }
 
